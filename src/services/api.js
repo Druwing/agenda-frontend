e314c5be0a41b4664945cb82257f9bee7706db82
Druwing <143631126+Druwing@users.noreply.github.com>
2025-05-17 09:00:37 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://jwtauthbackend-cloud.vercel.app';
 
 const api = axios.create({
   baseURL: API_URL,
